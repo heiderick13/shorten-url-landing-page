b7fe2d3cf4876c832f6fddd658cb312bd3bb18ca
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-06 20:00:50 -0300

    :sparkles: feat: form validation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,25 @@
 const openMenu = document.querySelector("#mobile-menu-btn");
 const mobileNav = document.querySelector(".mobile-nav-links");
+const shortenForm = document.querySelector("#shorten-form");
+const shortThis = document.querySelector("#short-this");
+const shortBtn = document.querySelector("#submit-btn");
 
 openMenu.addEventListener("click", () => {
   openMenu.classList.toggle("bi-list");
   openMenu.classList.toggle("bi-x-lg");
   mobileNav.classList.toggle("active");
+});
+
+shortBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let isEmpty = shortThis.value == "";
+
+  if (isEmpty) {
+    shortenForm.classList.add("error");
+    shortThis.classList.add("error");
+  } else {
+    shortenForm.classList.remove("error");
+    shortThis.classList.remove("error");
+  }
 });
