82df0f9d323e1353ca39e703d37a393b667a602e
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-07 17:36:57 -0300

    :sparkles: feat: show shortened url

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,7 @@ const mobileNav = document.querySelector(".mobile-nav-links");
 const shortenForm = document.querySelector("#shorten-form");
 const shortThis = document.querySelector("#short-this");
 const shortBtn = document.querySelector("#submit-btn");
+const statsSection = document.querySelector("#advanced-stats");
 const loadingElement = document.querySelector("#loading");
 const api = "https://api.shrtco.de/v2/shorten?url=";
 
@@ -12,15 +13,10 @@ async function getShorten() {
   let url = shortThis.value;
   let response = await fetch(api + url);
 
-  let data = await response
-    .json()
-    .then((data) => {
-      console.log(data.result.full_short_link2);
-      console.log(data.result.original_link);
-    })
-    .then(() => {
-      hideLoading();
-    });
+  let data = await response.json().then((data) => {
+    hideLoading();
+    addUrlElement(data.result.original_link, data.result.full_short_link2);
+  });
 }
 
 function showLoading() {
@@ -29,6 +25,50 @@ function showLoading() {
 
 function hideLoading() {
   loadingElement.classList.remove("show");
+}
+
+function generateId() {
+  return Date.now();
+}
+
+function addUrlElement(originalUrl, shortenedUrl) {
+  let urlObj = {
+    id: generateId(),
+    originalUrl: originalUrl,
+    shortenedUrl: shortenedUrl,
+  };
+
+  let urlEl = createUrlElement(
+    urlObj.id,
+    urlObj.originalUrl,
+    urlObj.shortenedUrl
+  );
+
+  statsSection.prepend(urlEl);
+}
+
+function createUrlElement(id, originalUrl, shortenedUrl, copied) {
+  let urlEl = document.createElement("div");
+
+  urlEl.classList.add("url-element");
+
+  let original = document.createElement("p");
+  original.classList.add("grow");
+  original.textContent = originalUrl;
+
+  let shortened = document.createElement("p");
+  shortened.classList.add("shortened");
+  shortened.textContent = shortenedUrl;
+
+  let copyBtn = document.createElement("button");
+  copyBtn.classList.add("btn");
+  copyBtn.textContent = "Copy";
+
+  urlEl.appendChild(original);
+  urlEl.appendChild(shortened);
+  urlEl.appendChild(copyBtn);
+
+  return urlEl;
 }
 
 // Event handlers
@@ -41,7 +81,6 @@ openMenu.addEventListener("click", () => {
 
 shortBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  getShorten();
 
   let isEmpty = shortThis.value == "";
 
@@ -51,5 +90,6 @@ shortBtn.addEventListener("click", (e) => {
   } else {
     shortenForm.classList.remove("error");
     shortThis.classList.remove("error");
+    getShorten();
   }
 });
